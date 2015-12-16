9e24f687ff7e41d4324cfcd91c394f2def6af77f
0rigen <christopher.parsons@jhuapl.edu>
2015-12-16 10:12:14 -0500

    Improvements to reliability over HTTP and HTTPS, including redirect handling

--- version_id.py
+++ version_id.py
@@ -1,5 +1,4 @@
 import logging
-import re
 
 import requests
 import url_processor
@@ -18,17 +17,7 @@ bold = "\033[1m"
 underline = "\033[4m"
 
 
-# SP Versions are identified in the response headers as 'MicrosoftSharePointTeamServices': 'X.X.X.X'
-# Identify SP versions via initial GET request
-# 2010 will start with 14, like 14.0.0.6010
-# 2013 will start with 15.
-# 2007 will start with 12.
-# 2003 will start with 6.
-
-# TODO Research what difference the version makes to interrogation functions.
-# TODO Different service names and standards, etc.
-# TODO Version ID fails over HTTPS - can I fix that?
-
+# TODO Research what difference the version makes to interrogation functions.  The results of this check may impact later requests
 
 ####################################################
 # identify                                         #
@@ -38,7 +27,6 @@ underline = "\033[4m"
 # @ port - the target port                         #
 ####################################################
 def identify(url, port=None):
-
     # Process the link, if port is specified
     if port is not None:
         link = url_processor.checkhttp(url, port)
@@ -46,32 +34,67 @@ def identify(url, port=None):
         link = url
 
     # Request the page
-    r = requests.get(link)
+    # HTTPS connections fail if there's a redirect, so jus take the first response.
+    # HTTP connections are ok with redirects, so they are flagged to allow.
+    if port == 443:
+        r = requests.head(link, headers={
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'})
+    elif port == 80:
+        r = requests.head(link, headers={
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'},
+                          allow_redirects=True)
 
-    # Check for a successful response
-    status_code = str(r.status_code)
-    success_code = re.match("(2)\w", status_code)  # Check for successful response code
-
-    if success_code is None:  # Return 'Unknown' if request unsuccessful
+    # Check for a successful response.  Redirection still contains the necessary headers
+    if str(r.status_code).startswith("4") or str(r.status_code).startswith("5"):
         print(
             yellow + "[!] Unsuccessful request when attempting to identify SP version.  Version remains Unknown..." + endc)
-        logging.info("Version ID failed; did not sp_match 2xx response regex")
+        logging.info("Version ID failed; Got a response %s" % str(r.status_code))
         return "Unknown"
 
     # Search for version info in headers
-    sp_match = r.headers['microsoftsharepointteamservices']
-    asp_match = r.headers['x-aspnet-version']
-    serv_match = r.headers['server']
-    health_match = r.headers['x-sharepointhealthscore']
+    # SP
+    if r.headers.__contains__("microsoftsharepointteamservices"):
+        sp_match = r.headers['microsoftsharepointteamservices']
+    else:
+        sp_match = None
+        logging.info("No SP version identified.")
+
+    # ASP
+    if r.headers.__contains__("x-aspnet-version"):
+        asp_match = r.headers['x-aspnet-version']
+    else:
+        asp_match = None
+        logging.info("ASP version ID failed; successful request but no version information found.")
+
+    # Server
+    if r.headers.__contains__("server"):
+        serv_match = r.headers['server']
+    else:
+        serv_match = None
+        logging.info("Server version ID failed; successful request but no version information found.")
+
+    # Health Score
+    if r.headers.__contains__("x-sharepointhealthscore"):
+        health_match = r.headers['x-sharepointhealthscore']
+    else:
+        health_match = None
+        logging.info("Health Score retrieval failed.")
 
     # Process SharePoint version
     if sp_match is None:  # No version info returned
         print(yellow + "[!] No SharePoint version information returned." + endc)
-        logging.info("Version ID failed; successful request but no version information found.")
+        logging.info("Version ID failed; successful request but no SP version information found.")
     else:
         ver = str(sp_match)  # Store the version info and return
         print(green + "\n[*] SharePoint version identified as " + bold + "%s;" % ver + endc),
         logging.info("SP Version ID successful. Found %s" % ver)
+
+        # SP Versions are identified in the response headers as 'MicrosoftSharePointTeamServices': 'X.X.X.X'
+        # Identify SP versions via initial GET request
+        # 2010 will start with 14, like 14.0.0.6010
+        # 2013 will start with 15.
+        # 2007 will start with 12.
+        # 2003 will start with 6.
         if ver.startswith("6"):
             print(green + bold + "SharePoint 2003" + endc)
         elif ver.startswith("14"):
@@ -82,27 +105,18 @@ def identify(url, port=None):
             print(green + bold + "SharePoint 2013" + endc)
 
     # Process ASP version
-    if asp_match is None:  # No version info returned
-        print(yellow + "[!] ASP version not identified." + endc)
-        logging.info("ASP version ID failed; successful request but no version information found.")
-    else:
+    if asp_match is not None:
         ver = str(asp_match)  # Store the version info and return
         print(green + "\n[*] ASP version identified as " + bold + "%s" % ver + endc)
         logging.info("ASP Version ID successful. Found %s" % ver)
 
     # Process Server version
-    if serv_match is None:  # No version info returned
-        print(yellow + "[!] No Server version information returned." + endc)
-        logging.info("Server version ID failed; successful request but no version information found.")
-    else:
+    if serv_match is not None:
         ver = str(serv_match)  # Store the version info and return
         print(green + "\n[*] Server version identified as " + bold + "%s" % ver + endc)
         logging.info("Server version ID successful. Found %s" % ver)
 
     # Print Health Score
-    if health_match is None:
-        print(yellow + "[!] No health score information returned." + endc)
-        logging.info("Health Score retrieval failed.")
-    else:
+    if health_match is not None:
         ver = str(health_match)  # Store the version info and return
         print(green + "\n[*] Health Score: " + bold + "%s" % ver + endc)
